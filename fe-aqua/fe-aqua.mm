1c0af9cbaf42c894cddfec1d121d14caf960ab64
Jeong YunWon <jeong@youknowone.org>
2009-12-26 02:56:12 +0900

    gtimer bugfix

--- fe-aqua/fe-aqua.mm
+++ fe-aqua/fe-aqua.mm
@@ -165,7 +165,7 @@ static int input_seq = 1;
 
 /////////////////////////////////////////////////////////////////////////////
 
-#define USE_GLIKE_TIMER 1
+#define USE_GLIKE_TIMER 0
 #if USE_GLIKE_TIMER
 #import "GLikeTimer.h"
 #else
