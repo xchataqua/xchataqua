022c7ac9565a4a0c6586109da5d703af4ae8446d
Jeong YunWon <jeong@youknowone.org>
2009-12-31 04:26:14 +0900

    restructure project files

--- tools/localization/001_derive_strings_from_base_nib.sh
+++ tools/localization/001_derive_strings_from_base_nib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PWD=`pwd`
 BASELOCALE='en'
-WORKDIR="../../fe-aqua/$BASELOCALE.lproj"
+WORKDIR="../../Localization/$BASELOCALE.lproj"
 PWDDIR="$PWD/lproj/$BASELOCALE"
 
 mkdir -p $PWDDIR
