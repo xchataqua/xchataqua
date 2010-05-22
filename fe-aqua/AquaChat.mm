be9d442a7b0678d576ac730b1740bbc990bfaa34
Terje Bless <link@pobox.com>
2010-05-22 14:24:51 +0200

    Hook up Download menu item.
    The “Download” menu item in the Help menu was pointing to the
    method do_goto_download() that was missing for some reason. This
    commit adds the missing method and collects the four related
    methods from the Help menu so they're near eachother in source.

--- fe-aqua/AquaChat.mm
+++ fe-aqua/AquaChat.mm
@@ -295,9 +295,35 @@ EventInfo text_event_info[NUM_XP];
 	[prefs_controller show];
 }
 
+/*
+ * These four methods are hooked up to menu entries in the Help menu and are
+ * used to open various relevant URLs (docs, homepage, etc.) in the default
+ * web browser.
+ *
+ */
+
+  // Open the X-Chat Aqua home page.
+- (void) do_homepage:(id) sender
+{
+  [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://xchataqua.sourceforge.net/"]];
+}
+
+  // Open the X-Chat Aqua download page (same as homepage for now).
+- (void) do_goto_download:(id) sender
+{
+	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.uplinklabs.net/~tycho/projects/xchat-aqua/"]];
+}
+
+  // Open the X-Chat Aqua Release Notes.
 - (void) do_release_notes:(id) sender
 {
 	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.uplinklabs.net/~tycho/projects/xchat-aqua/sparkle/rnotes.html"]];
+}
+
+  // Open the X-Chat Aqua online documentation.
+- (void) do_online_docs:(id) sender
+{
+  [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://xchataqua.sourceforge.net/docs/"]];
 }
 
 - (void) post_init
@@ -420,16 +446,6 @@ EventInfo text_event_info[NUM_XP];
         identd_start ();
     else
         identd_stop ();
-}
-
-- (void) do_online_docs:(id) sender
-{
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://xchataqua.sourceforge.net/docs/"]];
-}
-
-- (void) do_homepage:(id) sender
-{
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://xchataqua.sourceforge.net/"]];
 }
 
 - (void) do_load_plugin:(id) sender
