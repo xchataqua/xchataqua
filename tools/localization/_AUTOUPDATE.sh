7cd6fde49ae88ac9ae8c0c1c412cc75974a02d07
Jeong YunWon <jeong@youknowone.org>
2010-06-06 09:39:38 +0900

    make localizations to be generated automatically

--- tools/localization/_AUTOUPDATE.sh
+++ tools/localization/_AUTOUPDATE.sh
@@ -3,9 +3,9 @@
 echo "-generate .mo from .po with 'msgfmt'"
 bash po_to_mo.sh
 echo ""
-echo "-generate .strings from .po"
-ruby po_to_strings.rb
-echo ""
+#echo "-generate .strings from .po"
+#ruby po_to_strings.rb  this script has bug
+#echo ""
 echo "-copy .strings to Localization"
 ruby copy_strings.rb
 echo ""
