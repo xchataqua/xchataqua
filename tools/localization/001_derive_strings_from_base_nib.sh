bb80b706d2f6bfbc08342496b9d88896ddb95574
Jeong YunWon <jeong@youknowone.org>
2010-08-20 22:46:44 +0900

    faster localize script

--- tools/localization/001_derive_strings_from_base_nib.sh
+++ tools/localization/001_derive_strings_from_base_nib.sh
@@ -8,16 +8,16 @@ mkdir -p $PWDDIR
 cd $WORKDIR
 for xib in `ls *.xib`; do
 	if [ $PWDDIR/$xib.strings -nt $xib ]; then
-		continue;
+		continue # pass if old one
 	fi
-  cmd="ibtool --generate-stringsfile $xib.strings.utf16 $xib"
-  cmd2="iconv -f utf-16 -t utf-8 $xib.strings.utf16"
+	cmd="ibtool --generate-stringsfile $xib.strings.utf16 $xib"
+	cmd2="iconv -f utf-16 -t utf-8 $xib.strings.utf16"
 	if [ $DEBUG ]; then
 	  echo "$cmd ; $cmd2"
 	fi
-  $cmd
-  $cmd2 > $PWDDIR/$xib.strings
-	rm $xib.strings.utf16
+	$cmd
+	$cmd2 > $PWDDIR/$xib.strings
+	rm $xib.strings.utf16 # remove temporary utf16 file
 	echo -n .
 done
 echo ""
