f4ab791ab8e5cedbe0e07cd0763413b384cfd763
Jeong YunWon <jeong@youknowone.org>
2010-09-03 00:47:20 +0900

    fix SGDebug error

--- fe-aqua/SGBoxView.m
+++ fe-aqua/SGBoxView.m
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA */
 
-#define DEBUG_BOXVIEW
+#define DEBUG_BOXVIEW NO
 
 #include <stdarg.h>
 #import "SGBoxView.h"
