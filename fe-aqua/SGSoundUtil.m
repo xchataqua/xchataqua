9e326b1c11a3b73bdf59a5a4d6671ab0d55dbfd7
Jeong YunWon <jeong@youknowone.org>
2010-09-02 22:53:51 +0900

    rewrite for-loop

--- fe-aqua/SGSoundUtil.m
+++ fe-aqua/SGSoundUtil.m
@@ -4,25 +4,23 @@
 
 + (NSArray *) systemSounds
 {
-    NSMutableArray *paths = [[NSMutableArray arrayWithCapacity:0] retain];
-    
-    NSArray *dirs = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSAllDomainsMask, YES);
-                                                             
-    for (NSUInteger i = 0; i < [dirs count]; i ++)
-    {
-        NSString *lib_name = (NSString *) [dirs objectAtIndex:i];
-        NSString *dir_name = [NSString stringWithFormat:@"%@/Sounds", lib_name];
-        NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:dir_name error:NULL];
-        
-        for (NSUInteger j = 0; j < [files count]; j ++)
-        {
-            NSString *file = (NSString *) [files objectAtIndex:j];
-            NSString *s = [dir_name stringByAppendingFormat:@"/%@", file];
-            [paths addObject:s];
-        }
-    }
+	NSMutableArray *paths = [[NSMutableArray alloc] init];
+	
+	NSArray *directories = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSAllDomainsMask, YES);
+															 
+	for ( NSString *libraryName in directories )
+	{
+		NSString *directoryName = [NSString stringWithFormat:@"%@/Sounds", libraryName];
+		NSArray *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:directoryName error:NULL];
+		
+		for ( NSString *file in files )
+		{
+			NSString *path = [directoryName stringByAppendingFormat:@"/%@", file];
+			[paths addObject:path];
+		}
+	}
 
-    return paths;
+	return paths;
 }
 
 @end
