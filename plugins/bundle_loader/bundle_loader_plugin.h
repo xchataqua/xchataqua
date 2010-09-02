52f7a79727e66c74111a988eae379c92784a8eb5
Jeong YunWon <jeong@youknowone.org>
2010-09-02 21:33:02 +0900

    remove extern "C"

--- plugins/bundle_loader/bundle_loader_plugin.h
+++ plugins/bundle_loader/bundle_loader_plugin.h
@@ -1,3 +1,3 @@
 int bundle_loader_init (xchat_plugin *plugin_handle, char **plugin_name,
-                        char **plugin_desc, char **plugin_version, char *arg);
+						char **plugin_desc, char **plugin_version, char *arg);
 void bundle_loader_auto_load();
