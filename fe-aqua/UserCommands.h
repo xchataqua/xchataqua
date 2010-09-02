fc0c71e2861cc66b2b1b79aefcf1c1cfe817e2d8
Jeong YunWon <jeong@youknowone.org>
2010-09-03 00:19:19 +0900

    rename One..->..Item and rewrite with property

--- fe-aqua/UserCommands.h
+++ fe-aqua/UserCommands.h
@@ -15,8 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA */
 
-#import <Cocoa/Cocoa.h>
-
 @interface UserCommands : NSObject
 #if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
 <NSTableViewDelegate>
