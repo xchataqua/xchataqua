f2dccf780142078f46d40365d58d41f484ebf443
Jeong YunWon <jeong@youknowone.org>
2010-09-02 22:48:59 +0900

    rename method and variables

--- fe-aqua/SGRowColView.m
+++ fe-aqua/SGRowColView.m
@@ -23,111 +23,111 @@
 
 - (id) initWithFrame:(NSRect) frameRect
 {
-    [super initWithFrame:frameRect];
-    
-    self->rows = 0;
-    self->cols = 0;
-    
-    return self;
+	[super initWithFrame:frameRect];
+	
+	self->rows = 0;
+	self->cols = 0;
+	
+	return self;
 }
 
 - (void) dealloc
 {
-    [super dealloc];
+	[super dealloc];
 }
 
 - (void) do_layout
 {
-    if (cols == 0)
-        return;
-        
-    NSRect r = [self bounds];
+	if (cols == 0)
+		return;
+		
+	NSRect bounds = [self bounds];
 
-    // If we shrink (or expand) ourselvs in the horiontal, our width is the width of
-    // the widest row (using the preferred width of all sub views).
-        
-    if (shrinkh)
-    {
-        r.size.width = 0;
-        CGFloat thisw = 0;
-        
-        for (NSUInteger i = 0; i < [metaViews count]; i ++)
-        {
-            if (i % cols == 0)
-                thisw = 0;
-            
-            id metaView = [metaViews objectAtIndex:i];
-            NSRect b = [metaView prefSize];
-            
-            thisw += b.size.width;
-            
-            if (thisw > r.size.width)
-                r.size.width = thisw;
-        }
-    }
-    
-    NSUInteger actualRows = rows;
+	// If we shrink (or expand) ourselvs in the horiontal, our width is the width of
+	// the widest row (using the preferred width of all sub views).
+		
+	if (shrinkh)
+	{
+		bounds.size.width = 0;
+		CGFloat thisw = 0;
+		
+		for (NSUInteger i = 0; i < [metaViews count]; i ++)
+		{
+			if (i % cols == 0)
+				thisw = 0;
+			
+			id metaView = [metaViews objectAtIndex:i];
+			NSRect b = [metaView prefSize];
+			
+			thisw += b.size.width;
+			
+			if (thisw > bounds.size.width)
+				bounds.size.width = thisw;
+		}
+	}
+	
+	NSUInteger actualRows = rows;
 
-    if (actualRows == 0)
-        actualRows = ([metaViews count] + cols - 1) / cols;
-    
-    // And now for vertical
-    
-    if (shrinkv)
-    {
-        r.size.height = 0.0f;
-        CGFloat thish = 0.0f;
-        
-        for (NSUInteger i = 0; i < [metaViews count]; i ++)
-        {
-            if (i % actualRows == 0) 
+	if (actualRows == 0)
+		actualRows = ([metaViews count] + cols - 1) / cols;
+	
+	// And now for vertical
+	
+	if (shrinkv)
+	{
+		bounds.size.height = 0.0f;
+		CGFloat thish = 0.0f;
+		
+		for (NSUInteger i = 0; i < [metaViews count]; i ++)
+		{
+			if (i % actualRows == 0) 
 				thish = 0.0f;
-            
-            NSUInteger ii = (i * cols) % actualRows;
-            if (ii >= [metaViews count]) continue;
+			
+			NSUInteger ii = (i * cols) % actualRows;
+			if (ii >= [metaViews count]) continue;
 
-            id metaView = [metaViews objectAtIndex:ii];
-            NSRect b = [metaView prefSize];
+			id metaView = [metaViews objectAtIndex:ii];
+			NSRect b = [metaView prefSize];
 
-            thish += b.size.height;
-            
-            if (thish > r.size.height)
-                r.size.height = thish;
-        }
-    }
+			thish += b.size.height;
+			
+			if (thish > bounds.size.height)
+				bounds.size.height = thish;
+		}
+	}
 
-    CGFloat vh = r.size.height / actualRows;
-    CGFloat vw = r.size.width / cols;
-    
-    for (NSUInteger i = 0; i < [metaViews count]; i ++)
-    {
-        id metaView = [metaViews objectAtIndex:i];
-        
-        NSRect b;
-    
-        b.origin.x = r.origin.x + floor ((i % cols) * vw);
-        b.origin.y = r.origin.y + floor ((i / cols) * vh);
-        b.size.width = floor (vw);
-        b.size.height = floor (vh);
+	CGFloat vh = bounds.size.height / actualRows;
+	CGFloat vw = bounds.size.width / cols;
+	
+	for (NSUInteger i = 0; i < [metaViews count]; i ++)
+	{
+		id metaView = [metaViews objectAtIndex:i];
+		
+		NSRect b;
+	
+		b.origin.x = bounds.origin.x + floor ((i % cols) * vw);
+		b.origin.y = bounds.origin.y + floor ((i / cols) * vh);
+		b.size.width = floor (vw);
+		b.size.height = floor (vh);
 
-        [metaView setFrame:b];
-    }
-    
-    [self setFrameSize:r.size];
+		[metaView setFrame:b];
+	}
+	
+	[self setFrameSize:bounds.size];
 }
 
-- (void)setCols:(NSInteger)new_cols rows:(NSInteger)new_rows
+- (void)setColumns:(NSInteger)new_cols rows:(NSInteger)new_rows
 {
-    self->rows = new_rows;
-    self->cols = new_cols;
-    [self queue_layout];
+	self->rows = new_rows;
+	self->cols = new_cols;
+	[self queue_layout];
 }
 
 - (void)setShrinkHoriz:(BOOL)new_shrinkh vert:(BOOL)new_shrinkv
 {
-    self->shrinkh = new_shrinkh;
-    self->shrinkv = new_shrinkv;
-    [self queue_layout];
+	self->shrinkh = new_shrinkh;
+	self->shrinkv = new_shrinkv;
+	[self queue_layout];
 }
 
 @end
