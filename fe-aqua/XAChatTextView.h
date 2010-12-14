920dd1363f7afc1e11de836d4fdf67eab0f13093
Jeong YunWon <jeong@youknowone.org>
2010-12-15 02:02:14 +0900

    drop jaguar support

--- fe-aqua/XAChatTextView.h
+++ fe-aqua/XAChatTextView.h
@@ -15,8 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA */
 
-//////////////////////////////////////////////////////////////////////
-
 /* CL: these methods are used in the default key bindings, are implemented by Cocoa classes,
 	   but Apple forgot to include them in the headers. Declare them so the compiler knows
 	   what we're calling. */
@@ -25,7 +23,7 @@
 - (void)scrollToEndOfDocument:(id)sender;
 @end
 
-//////////////////////////////////////////////////////////////////////
+#pragma mark -
 
 @class ColorPalette;
 @class ChatWindow;
