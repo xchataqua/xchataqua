5f550198c22f1b05bee217580b5eb4b429ff09b5
Jeong YunWon <jeong@youknowone.org>
2011-06-30 09:37:55 +0900

    fix version check bug
    'if' should be used but 'ifdef' been.

--- Palette/SGHBoxViewInspector.h
+++ Palette/SGHBoxViewInspector.h
@@ -1,7 +1,7 @@
 /* SGHBoxViewInspector */
 
 
-#ifdef MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_5
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_5
 # import <InterfaceBuilderKit/InterfaceBuilderKit.h>
 #else
 # import <InterfaceBuilder/InterfaceBuilder.h>
@@ -9,8 +9,8 @@
 
 @interface SGHBoxViewInspector : IBInspector
 {
-	IBOutlet NSPopUpButton *HJustMenu;
-	IBOutlet NSTextField *inner_text;
-	IBOutlet NSTextField *outter_text;
+    IBOutlet NSPopUpButton *HJustMenu;
+    IBOutlet NSTextField *inner_text;
+    IBOutlet NSTextField *outter_text;
 }
 @end
