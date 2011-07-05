6041d7a9380e9fb426433ddffdcdf074819d7f46
Jeong YunWon <developer@youknowone.org>
2011-07-05 17:06:13 +0900

    fix shell path to avoid whitespace path error

--- tools/localization/po_to_mo.sh
+++ tools/localization/po_to_mo.sh
@@ -10,7 +10,7 @@ MSGFMT="$PROJECT_DIR/contrib/bin/msgfmt"
 
 moname=xchat
 
-for pofile in `ls $PO_DIR/*.po`; do
+for pofile in `ls "$PO_DIR/*.po"`; do
 	locale=`basename $pofile .po`
 	modir="$MO_BASE_DIR/$locale/LC_MESSAGES"
 	mofile="$modir/$moname.mo"
