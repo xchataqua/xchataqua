5a7dc052d8e6c88b4c238bfa90715da13c397b97
Jeong YunWon <jeong@youknowone.org>
2011-08-19 16:52:32 +0900

    Prebuild before building product

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release'
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release'
 cd '../build/Release'
 productbuild --component 'XChat Azure.app' '/Applications' --sign "3rd Party Mac Developer Installer: 3rddev Inc." 'XChat Azure.pkg'
