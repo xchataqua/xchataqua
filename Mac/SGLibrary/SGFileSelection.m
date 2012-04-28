2bd08d421f92c502e860db804aeaa34462d412d4
Jeong YunWon <jeong@youknowone.org>
2012-04-28 20:28:24 +0900

    Fix sendfile problems
    Remove Deprecated methods
    issue xchataqua/xchatazure #24

--- Mac/SGLibrary/SGFileSelection.m
+++ Mac/SGLibrary/SGFileSelection.m
@@ -19,126 +19,135 @@
 
 static NSString *fixPath (NSString *path)
 {
-	if ( path == nil ) return nil;
-		
-	if ( [path isAbsolutePath] ) return path;
-	
-	// Assume it's relative to the dir with the app bundle
-	return [NSString stringWithFormat:@"%@/../%@", [[NSBundle mainBundle] bundlePath], path];
+    if ( path == nil ) return nil;
+        
+    if ( [path isAbsolutePath] ) return path;
+    
+    // Assume it's relative to the dir with the app bundle
+    return [NSString stringWithFormat:@"%@/../%@", [[NSBundle mainBundle] bundlePath], path];
 }
 
 @implementation SGFileSelection
 
 + (NSString *) selectWithWindow:(NSWindow *) win
 {
-	return [self selectWithWindow:win inDirectory:nil];
+    return [self selectWithWindow:win inDirectory:nil];
 }
 
 + (NSString *) selectWithWindow:(NSWindow *) win inDirectory:(NSString *) dir
 {
-	dir = fixPath (dir);
-	
-	NSOpenPanel *panel = [NSOpenPanel openPanel];
+    dir = fixPath (dir);
+    
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
 
-	[panel setCanChooseFiles:YES];
-	[panel setResolvesAliases:NO];
-	[panel setCanChooseDirectories:NO];
-	[panel setAllowsMultipleSelection:NO];
+    [panel setCanChooseFiles:YES];
+    [panel setResolvesAliases:NO];
+    [panel setCanChooseDirectories:NO];
+    [panel setAllowsMultipleSelection:NO];
 
-	NSInteger sts;
+    if (dir) {
+        [panel setDirectoryURL:[NSURL fileURLWithPath:dir isDirectory:YES]];
+    }
+    
+    NSInteger sts;
+    if (win)
+    {
+        SInt32 version = 0;
+        Gestalt(gestaltSystemVersion, &version);
+        if ( version > 0x1050 ) { // snow leopard
+            [panel beginSheetModalForWindow:win completionHandler:NULL];
+            sts = [panel runModal];
+            [NSApp endSheet:panel];
+        } else {
+            // ignore deprecated warning. this is legacy support runtime code.
+            [panel beginSheetForDirectory:dir file:nil types:nil modalForWindow:win
+                        modalDelegate:nil didEndSelector:nil contextInfo:nil];
+            sts = [NSApp runModalForWindow:panel];
+            [NSApp endSheet:panel];
+        }
+    }
+    else {
+        sts = [panel runModal] == NSOKButton;
+    }
+    
+    [panel orderOut:self];
 
-	if (win)
-	{
-		SInt32 version = 0;
-		Gestalt(gestaltSystemVersion, &version);
-		if ( version > 0x1050 ) { // snow leopard
-			sts = [panel runModalForDirectory:dir file:nil types:nil] == NSOKButton;
-		} else {
-			[panel beginSheetForDirectory:dir file:nil types:nil modalForWindow:win
-						modalDelegate:nil didEndSelector:nil contextInfo:nil];
-			sts = [NSApp runModalForWindow:panel];
-			[NSApp endSheet:panel];
-		}
-	}
-	else
-		sts = [panel runModalForDirectory:dir file:nil types:nil] == NSOKButton;
-	
-	[panel orderOut:self];
+    if (sts)
+    {
+        NSURL *URL = [[panel URLs] objectAtIndex:0];
+        return URL.path;
+        
+    }
 
-	if (sts)
-	{
-		return [[panel filenames] objectAtIndex:0];
-	}
-
-	return nil;
+    return nil;
 }
 
 + (NSString *) saveWithWindow:(NSWindow *) win
 {
-	NSSavePanel *p = [NSSavePanel savePanel];
-	
-	[p setPrompt:NSLocalizedStringFromTable(@"Select", @"xchataqua", @"")];
-		
-	[p beginSheetForDirectory:nil file:nil
-	   modalForWindow:win modalDelegate:nil didEndSelector:nil
-	   contextInfo:nil];
+    NSSavePanel *p = [NSSavePanel savePanel];
+    
+    [p setPrompt:NSLocalizedStringFromTable(@"Select", @"xchataqua", @"")];
+        
+    [p beginSheetForDirectory:nil file:nil
+       modalForWindow:win modalDelegate:nil didEndSelector:nil
+       contextInfo:nil];
 
-	NSInteger sts = [NSApp runModalForWindow:p];
+    NSInteger sts = [NSApp runModalForWindow:p];
 
-	[NSApp endSheet:p];
-	
-	[p orderOut:self];
+    [NSApp endSheet:p];
+    
+    [p orderOut:self];
 
-	if (sts)
-		return [p filename];
-	
-	return nil;
+    if (sts)
+        return [p filename];
+    
+    return nil;
 }
 
 + (void) getFile:(NSString *)title initial:(NSString *)initial callback:(callback_t)callback userdata:(void *)userdata flags:(int)flags
 {
-	id panel;
-	BOOL dir=NO;
-	
-	if(flags & FRF_WRITE)
-		panel=[NSSavePanel savePanel];
-	else
-		panel=[NSOpenPanel openPanel];
-	
-	[panel setTitle:title];
-	if(initial)
-		[panel setDirectory:initial];
-	if(flags & FRF_MULTIPLE)
-		[panel setAllowsMultipleSelection:YES];
-	if(flags & FRF_CHOOSEFOLDER)
-		dir=YES;
-	[panel setCanChooseDirectories:dir];
-	[panel setCanChooseFiles:!dir];
-	
-	[panel beginSheetForDirectory:nil file:nil
-				   modalForWindow:nil modalDelegate:nil didEndSelector:nil
-					  contextInfo:nil];
-	
-	NSInteger sts = [NSApp runModalForWindow:panel];
-	
-	[NSApp endSheet:panel];
-	
-	[panel orderOut:self];
-	
-	if (sts)
-	{
-		if(flags & FRF_MULTIPLE)
-		{
-			NSArray *filenames=[panel filenames];
-			for(NSUInteger i=0;i<[filenames count];++i)
-			{
-				callback(userdata, (char *) [[filenames objectAtIndex:i] UTF8String]);
-			}
-			callback(userdata, 0);
-		}else
-			callback(userdata, (char *) [[panel filename] UTF8String]);
-	}else
-		callback(userdata, 0);
+    id panel;
+    BOOL dir=NO;
+    
+    if(flags & FRF_WRITE)
+        panel=[NSSavePanel savePanel];
+    else
+        panel=[NSOpenPanel openPanel];
+    
+    [panel setTitle:title];
+    if(initial)
+        [panel setDirectory:initial];
+    if(flags & FRF_MULTIPLE)
+        [panel setAllowsMultipleSelection:YES];
+    if(flags & FRF_CHOOSEFOLDER)
+        dir=YES;
+    [panel setCanChooseDirectories:dir];
+    [panel setCanChooseFiles:!dir];
+    
+    [panel beginSheetForDirectory:nil file:nil
+                   modalForWindow:nil modalDelegate:nil didEndSelector:nil
+                      contextInfo:nil];
+    
+    NSInteger sts = [NSApp runModalForWindow:panel];
+    
+    [NSApp endSheet:panel];
+    
+    [panel orderOut:self];
+    
+    if (sts)
+    {
+        if(flags & FRF_MULTIPLE)
+        {
+            NSArray *filenames=[panel filenames];
+            for(NSUInteger i=0;i<[filenames count];++i)
+            {
+                callback(userdata, (char *) [[filenames objectAtIndex:i] UTF8String]);
+            }
+            callback(userdata, 0);
+        }else
+            callback(userdata, (char *) [[panel filename] UTF8String]);
+    }else
+        callback(userdata, 0);
 }
 
 
