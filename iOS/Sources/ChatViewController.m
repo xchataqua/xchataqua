3d6bf0ecc6827aaf4f847948500c32365c34b685
Jeong YunWon <jeong@youknowone.org>
2012-05-05 17:41:31 +0900

    Replace glib to common one with libintl

--- iOS/Sources/ChatViewController.m
+++ iOS/Sources/ChatViewController.m
@@ -20,9 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA */
 
-#include "common/xchat.h"
-#include "common/xchatc.h"
-#include "common/outbound.h"
+#include "outbound.h"
 
 #include "fe-iphone_common.h"
 
--- iOS/Sources/XACommon.m
+++ iOS/Sources/XACommon.m
@@ -15,11 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA */
 
-#include "common/xchat.h"
-#include "common/xchatc.h"
-#include "common/outbound.h"
-#include "common/userlist.h"
-#include "common/server.h"
+#include "outbound.h"
+#include "userlist.h"
+#include "server.h"
 
 #include "XACommon.h"
 
