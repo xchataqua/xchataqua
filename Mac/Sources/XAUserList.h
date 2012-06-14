4f7975779f252f726a44e50b8a7413bb02979f03
Jeong YunWon <jeong@youknowone.org>
2012-06-14 15:07:13 +0900

    split MyUserList from ChatWindow to XAUserList

---
+++ Mac/Sources/XAUserList.h
@@ -0,0 +1,11 @@
+//
+//  XAUserList.h
+//  XChatAqua
+//
+//  Created by Jeong YunWon on 12. 6. 14..
+//  Copyright (c) 2012 youknowone.org All rights reserved.
+//
+
+@interface XAUserList : NSTableView
+
+@end
