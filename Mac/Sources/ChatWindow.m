3bb9242a1625f4e882609971c1260f6868e0a9f1
Jeong YunWon <jeong@youknowone.org>
2012-06-14 15:07:13 +0900

    split MyUserList from ChatWindow to XAUserList

--- Mac/Sources/ChatWindow.m
+++ Mac/Sources/ChatWindow.m
@@ -328,36 +328,6 @@
 
 #pragma mark -
 
-/* CL */
-@interface MyUserList : NSTableView
-/* CL end */
-@end
-
-@implementation MyUserList
-
-- (void)rightMouseDown:(NSEvent *)theEvent
-{
-    NSInteger clickedRow = [self rowAtPoint:[self convertPoint:[theEvent locationInWindow] fromView:nil]];
-    if (![self isRowSelected:clickedRow])
-        [self selectRowIndexes:[NSIndexSet indexSetWithIndex:clickedRow] byExtendingSelection:NO];
-    [super rightMouseDown:theEvent];
-}
-
-/* CL: let the delegate handle this */
-- (NSMenu *)menuForEvent:(NSEvent *)theEvent
-{
-    NSIndexSet *selectedRows = [self selectedRowIndexes];
-    if ([selectedRows count] == 0) return [super menuForEvent:theEvent];
-    ChatWindow *delegate = (id)[self delegate];
-    if ([delegate respondsToSelector:@selector(menuForEvent:rowIndexes:)])
-        return [delegate menuForEvent:theEvent rowIndexes:selectedRows];
-    else return [super menuForEvent:theEvent];
-}
-
-@end
-
-#pragma mark -
-
 @interface ChannelUser : NSObject
 {
 @public
