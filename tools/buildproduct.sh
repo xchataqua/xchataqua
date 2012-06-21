c9cfa6725862963eab85b412134ed0394b4b9800
Jeong YunWon <jeong@youknowone.org>
2012-06-21 23:55:01 +0900

    Add autogenerated buildnums for Azure

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+version=`git describe --tags`
+version=${version#appstore-}
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release' && \
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release' && \
 cd '../build/Release' && \
-productbuild --component 'XChat Azure.app' '/Applications' --sign "3rd Party Mac Developer Installer: 3rddev Inc." 'XChat Azure.pkg'
+productbuild --component "XChat Azure.app" '/Applications' --sign "3rd Party Mac Developer Installer: 3rddev Inc." "XChat Azure $version.pkg"
 
