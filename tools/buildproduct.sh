a67ae6d662dfbf91301e73e4ec5401a66e4fc34e
Jeong YunWon <jeong@youknowone.org>
2012-06-25 01:47:17 +0900

    Force regen Info.plist

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 version=`git describe --tags`
 version=${version#appstore-}
+rm ../build/Release/*.app/Contents/Info.plist
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release' && \
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release' && \
 cd '../build/Release' && \
