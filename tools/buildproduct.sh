a55782fb2592d809c199ea269ba82076f5820d62
Jeong YunWon <jeong@youknowone.org>
2012-08-02 01:00:54 +0900

    Try XPC to be signed properly

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -4,7 +4,6 @@ version=${version#appstore-}
 rm ../build/Release/*.app/Contents/Info.plist
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release' && \
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release' && \
-xcodebuild -project '../XChatAqua.xcodeproj' -target 'Azure Growl' -configuration 'Release' && \
 cd '../build/Release' && \
 productbuild --component "XChat Azure.app" '/Applications' --sign "3rd Party Mac Developer Installer: 3rddev Inc." "XChat Azure $version.pkg"
 
