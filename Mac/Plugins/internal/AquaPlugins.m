19351bdf8dca0313d009521ebf36da8a51715fbb
Terje Bless <link@pobox.com>
2012-09-29 11:05:38 +0200

    Add some source code comments to SGRegex
    SGRegex is using the old POSIX-ish regex functions (non-Unicode safe etc.)
    and is only used once in the XCA codebase (in AquaPlugins.m:fix_url()).
    Make a note of this and a TODO item to phase it out in favor of
    NSRegularExpression once we set our Deployment Target to 10.7.

--- Mac/Plugins/internal/AquaPlugins.m
+++ Mac/Plugins/internal/AquaPlugins.m
@@ -85,7 +85,9 @@ applescript_cb (char *word[], char *word_eol[], void *userdata)
 static NSString *fix_url (const char *url)
 {
     NSString *ret = [NSString stringWithUTF8String:url];
-    
+
+    // TODO: Replace this with NSRegularExpression or Data Detectors
+    // (and phase out SGRegex; this is its only use in XCA)
     SGRegex *regex = [SGRegex
                       regexWithString:@"(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?"
                       nSubExpr:2];
