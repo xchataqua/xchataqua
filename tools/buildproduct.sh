509881d82458907191236e22d1c91883de6dc9eb
Jeong YunWon <jeong@youknowone.org>
2014-06-12 22:59:04 +0900

    unify perl plugin

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -6,5 +6,5 @@ rm ../build/Release/*.app/Contents/Info.plist
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release' && \
 xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release' && \
 cd '../build/Release' && \
-productbuild --component "XChat Azure.app" '/Applications' --sign "3rd Party Mac Developer Installer: 3rddev Inc." "XChat Azure $version.pkg"
+productbuild --component "XChat Azure.app" '/Applications' --sign "3rd Party Mac Developer Installer: YunWon Jeong" "XChat Azure $version.pkg"
 
