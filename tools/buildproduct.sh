1d5079638478ec6ce0379003f6f774041c0af4a0
Jeong YunWon <jeong@youknowone.org>
2017-01-27 01:00:54 +0900

    Fix deployment settings

--- tools/buildproduct.sh
+++ tools/buildproduct.sh
@@ -3,8 +3,8 @@ PWD=`pwd`
 version=`git describe --tags`
 version=${version#appstore-}
 rm ../build/Release/*.app/Contents/Info.plist
-xcodebuild -project '../XChatAqua.xcodeproj' -target 'Prebuild' -configuration 'Release' && \
-xcodebuild -project '../XChatAqua.xcodeproj' -target 'XChat Azure' -configuration 'Release' && \
+xcodebuild -workspace '../XChatAqua.xcworkspace' -scheme 'Prebuild' -configuration 'Release' && \
+xcodebuild -workspace '../XChatAqua.xcworkspace' -scheme 'XChat Azure' -configuration 'Release' && \
 cd '../build/Release' && \
 productbuild --component "XChat Azure.app" '/Applications' --sign "3rd Party Mac Developer Installer: YunWon Jeong" "XChat Azure $version.pkg"
 
